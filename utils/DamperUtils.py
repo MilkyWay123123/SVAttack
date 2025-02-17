0b57e1f03d2cdfe25c3a02609afd4f35726af175
MilkyWay123123 <zouhuanxin99@gmail.com>
2025-02-17 09:09:28 +0800

    init

--- utils/DamperUtils.py
+++ utils/DamperUtils.py
@@ -1,16 +1,13 @@
-'''
-阻尼器
-'''
 import torch
 
 hookratio = 0.1
 
 
-# 注册钩子
 def register_hooks(classifier, ratio, model_name):
+    global hookratio
     hookratio = ratio
     if model_name == 'stgcn' or model_name == 'stgcn120':
-        print('注册stgcn阻尼器')
+        print('Register the stgcn damper')
         for name, layer in classifier.named_modules():  # stgcn
             if ('module.st_gcn_networks.4.gcn' in name or 'module.st_gcn_networks.5.gcn' in name
                     or 'module.st_gcn_networks.6.gcn' in name or 'module.st_gcn_networks.7.gcn' in name
@@ -18,14 +15,14 @@ def register_hooks(classifier, ratio, model_name):
                 print(f'stgcn={name}')
                 hook = layer.register_full_backward_hook(adjust_array)
     elif model_name == 'agcn' or model_name == 'agcn120':
-        print('注册agcn阻尼器')
+        print('Register the agcn damper')
         for name, layer in classifier.named_modules():  # agcn
             if ('l4.gcn1' in name or 'l5.gcn1' in name or 'l6.gcn1' in name or 'l7.gcn1' == name or 'l8.gcn1' in name
                     or 'l9.gcn1' in name or 'l10.gcn1' in name):
                 print(f'agcn={name}')
                 hook = layer.register_full_backward_hook(adjust_array)
     elif model_name == 'ctrgcn' or model_name == 'ctrgcn120':
-        print('注册ctrgcn阻尼器')
+        print('Register the ctrgcn damper')
         for name, layer in classifier.named_modules():  # ctrgcn
             if (
                     'l4.gcn' in name or 'l5.gcn' in name or 'l6.gcn' in name or 'l7.gcn' in name or 'l8.gcn' in name or 'l9.gcn' in name or 'l10.gcn' in name):
@@ -33,17 +30,18 @@ def register_hooks(classifier, ratio, model_name):
                 hook = layer.register_full_backward_hook(adjust_array)
     else:
         print('-----------------------------------------')
-        print('未注册阻尼器')
+        print('Unregistered damper')
         print('-----------------------------------------')
 
 
 def adjust_array(module, grad_input, grad_output):
-    result_tensor = []
     for i in range(len(grad_input)):
         data = grad_input[i]
         data_flat = data.reshape(-1)
         size = len(data_flat)
         topk_values, topk_indices = torch.topk(data_flat, int(size * 0.9))
         data_flat[topk_indices] *= hookratio
-        result_tensor.append(torch.reshape(data_flat, data.shape))
-    return result_tensor
+        data_flat = torch.reshape(data_flat, data.shape)
+
+        grad_input[i].data = data_flat
+
